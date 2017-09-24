b21f01861211aea79f34598d1529e32bb538a608
Dudley Dawes <dudley2.dawes@live.uwe.ac.uk>
2017-09-24 01:29:21 +0100

    removed sentence generator project

--- LevelDesignConsoleTest/main.cpp
+++ LevelDesignConsoleTest/main.cpp
@@ -5,162 +5,15 @@
 */
 
 
-#include <iostream>
-#include "Generator.h"
 
 
-int main()
-{
-	Generator gen;
-	while(true)
-	{
-		std::cout << "..................................." << std::endl;
-		std::cout << gen.generate() << std::endl;
-		std::cout << "..................................." << std::endl;
-		system("PAUSE");
-		std::cout << std::endl;
-	}
-	return 0;
-}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-/*
 #include <iostream>
 #include <Windows.h>
 
 
-#include <JumpRule.h>
+#include "JumpRule.h"
 
 
-// CODE FOR LEVELDESIGNERPROJECT
 int main()
 {
 	unsigned short length = 0;
@@ -219,4 +72,3 @@ int main()
 
 	return 0;
 }
-*/
